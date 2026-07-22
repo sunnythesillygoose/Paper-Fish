dc97459d28ce7a9d7b97a72850a85ba38ea395f6
Sunny <o.puskas@hbk-bs.de>
2026-07-23 01:02:47 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,185 @@
+function show1() {
+   fish1.classList.toggle("hide");
+   button1.classList.toggle("activated");
+   m11.classList.toggle("hide");
+   m12.classList.toggle("hide");
+}
+
+function show2() {
+   fish2.classList.toggle("hide");
+   button2.classList.toggle("activated");
+   m21.classList.toggle("hide");
+   m22.classList.toggle("hide");
+}
+
+function show3() {
+   fish3.classList.toggle("hide");
+   button3.classList.toggle("activated");
+   m31.classList.toggle("hide");
+   m32.classList.toggle("hide");
+}
+
+function show4() {
+   fish4.classList.toggle("hide");
+   button4.classList.toggle("activated");
+   m41.classList.toggle("hide");
+   m42.classList.toggle("hide");
+}
+
+function show5() {
+   fish5.classList.toggle("hide");
+   button5.classList.toggle("activated");
+   m51.classList.toggle("hide");
+   m52.classList.toggle("hide");
+}
+
+function show6() {
+   fish6.classList.toggle("hide");
+   button6.classList.toggle("activated");
+   m61.classList.toggle("hide");
+   m62.classList.toggle("hide");
+}
+
+function show7() {
+   fish7.classList.toggle("hide");
+   button7.classList.toggle("activated");
+   m71.classList.toggle("hide");
+   m72.classList.toggle("hide");
+}
+
+function show8() {
+   fish8.classList.toggle("hide");
+   button8.classList.toggle("activated");
+   m81.classList.toggle("hide");
+   m82.classList.toggle("hide");
+}
+
+function show9() {
+   fish9.classList.toggle("hide");
+   button9.classList.toggle("activated");
+   m91.classList.toggle("hide");
+   m92.classList.toggle("hide");
+}
+
+function show10() {
+   fish10.classList.toggle("hide");
+   button10.classList.toggle("activated");
+   m101.classList.toggle("hide");
+   m102.classList.toggle("hide");
+}
+
+function show11() {
+   fish11.classList.toggle("hide");
+   button11.classList.toggle("activated");
+   m111.classList.toggle("hide");
+   m112.classList.toggle("hide");
+}
+
+function show12() {
+   fish12.classList.toggle("hide");
+   button12.classList.toggle("activated");
+   m121.classList.toggle("hide");
+   m122.classList.toggle("hide");
+}
+
+function show13() {
+   fish13.classList.toggle("hide");
+   button13.classList.toggle("activated");
+   m131.classList.toggle("hide");
+   m132.classList.toggle("hide");
+}
+
+function show14() {
+   fish14.classList.toggle("hide");
+   button14.classList.toggle("activated");
+   m141.classList.toggle("hide");
+   m142.classList.toggle("hide");
+}
+
+function show15() {
+   fish15.classList.toggle("hide");
+   button15.classList.toggle("activated");
+   m151.classList.toggle("hide");
+   m152.classList.toggle("hide");
+}
+
+function show16() {
+   fish16.classList.toggle("hide");
+   button16.classList.toggle("activated");
+   m161.classList.toggle("hide");
+   m162.classList.toggle("hide");
+}
+
+function show17() {
+   fish17.classList.toggle("hide");
+   button17.classList.toggle("activated");
+   m171.classList.toggle("hide");
+   m172.classList.toggle("hide");
+}
+
+function show18() {
+   fish18.classList.toggle("hide");
+   button18.classList.toggle("activated");
+   m181.classList.toggle("hide");
+   m182.classList.toggle("hide");
+}
+
+function show19() {
+   fish19.classList.toggle("hide");
+   button19.classList.toggle("activated");
+   m191.classList.toggle("hide");
+   m192.classList.toggle("hide");
+}
+
+function show20() {
+   fish20.classList.toggle("hide");
+   button20.classList.toggle("activated");
+   m201.classList.toggle("hide");
+   m202.classList.toggle("hide");
+}
+
+function show29() {
+   fish29.classList.toggle("hide");
+   button29.classList.toggle("activated");
+   m291.classList.toggle("hide");
+   m292.classList.toggle("hide");
+}
+
+function show43() {
+   fish43.classList.toggle("hide");
+   button43.classList.toggle("activated");
+   m431.classList.toggle("hide");
+   m432.classList.toggle("hide");
+}
+
+function show46() {
+   fish46.classList.toggle("hide");
+   button46.classList.toggle("activated");
+   m461.classList.toggle("hide");
+   m462.classList.toggle("hide");
+}
+
+function show49() {
+   fish49.classList.toggle("hide");
+   button49.classList.toggle("activated");
+   m491.classList.toggle("hide");
+   m492.classList.toggle("hide");
+}
+
+function show50() {
+   fish50.classList.toggle("hide");
+   button50.classList.toggle("activated");
+   m501.classList.toggle("hide");
+   m502.classList.toggle("hide");
+}
+
+function show51() {
+   fish51.classList.toggle("hide");
+   button51.classList.toggle("activated");
+   m511.classList.toggle("hide");
+   m512.classList.toggle("hide");
+}
+
+function hidecontrols() {
+  controls.classList.toggle("hide");
+}
